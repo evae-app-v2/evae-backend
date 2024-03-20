1774b6fd8d0d272531e12e540f6ae1d5d5033d36
Mamadou SIDIBE <mamadousidibe.3116@gmail.com>
2024-03-20 12:32:54 +0100

    Commentaire

---
+++ src/main/java/com/example/demo/DTO/ReponseEvaluationOutputDTO.java
@@ -0,0 +1,12 @@
+package com.example.demo.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReponseEvaluationOutputDTO {
+
+    private boolean estRepondre;
+    private String commentaire;
+}
